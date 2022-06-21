0280c47e70dc3c3d4276805ababbbf6af5a0b068
Kiah Hewitt <kiah.hewitt@capco.com>
2022-06-21 12:02:35 +0100

    initial test structure

--- src/containers/App/_tests_/App.test.js
+++ src/containers/App/_tests_/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders Capco Engineering Website text', () => {
   render(<App />);
   const linkElement = screen.getByText(/Capco Engineering Website/i);
   expect(linkElement).toBeInTheDocument();
--- src/index.js
+++ src/index.js
@@ -10,5 +10,3 @@ root.render(
   </React.StrictMode>
 );
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-
